188b101bc766ae4aefdd4fe0d9e64e19b55a5b7d
StefanDass <stefan.dass13@gmail.com>
2021-10-29 21:47:35 -0500

    changes made

--- src/components/RecommendationDetails.js
+++ src/components/RecommendationDetails.js
@@ -1,19 +1,10 @@
 import React from 'react';
 
-const RecommendationDetails = ({ price, likes, dislikes, ingredients }) => {
-    // possible ingredients destructuring here:
-    const first = ingredients['1'];
-    const second = ingredients['2'];
-    const third = ingredients['3'];
-    const fourth = ingredients['4'];
-
+const RecommendationDetails = ({ name }) => {
+    
     return (
         <>
-            <h5>Price: {price}</h5>
-            <h5>
-                Made with {first}, {second}, {third} and {fourth}
-            </h5>
-            <h5>likes: {likes}  dislikes: {dislikes}</h5>
+            <h5>We want: {name}</h5>
         </>
     );
 };
